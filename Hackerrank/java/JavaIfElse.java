56f6d852e9d30355c1f508ff9d72c730d49b51b3
ahmedgodaa <ana.a7med1140@gmail.com>
2022-02-03 01:44:37 +0200

    add all codes to intellij

--- Hackerrank/java/JavaIfElse.java
+++ Hackerrank/java/JavaIfElse.java
@@ -1,35 +1,30 @@
-package Hackerrank;
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
+package Hackerrank.java;
+
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 
 public class JavaIfElse {
-
 
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int n = scanner.nextInt();
-        if (n % 2 != 0 ) {
+        if (n % 2 != 0) {
             System.out.println("Weird");
         } else {
-            if (n <= 5 && n >= 2){
+            if (n <= 5 && n >= 2) {
                 System.out.println("Not Weird");
             }
-            if(n <= 20 && n >= 6){
+            if (n <= 20 && n >= 6) {
                 System.out.println("Weird");
 
             }
-            if (n > 20){
+            if (n > 20) {
                 System.out.println("Not Weird");
             }
         }
-        
+
 
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
